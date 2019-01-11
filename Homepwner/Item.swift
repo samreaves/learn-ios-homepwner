e81a7b799f4c0cc9e5bbbf828d706b820db4902e
Sam <thesamreaves@gmail.com>
2019-01-10 22:12:55 -0500

    Added custom UITableViewController, custom item class

---
+++ Homepwner/Item.swift
@@ -0,0 +1,47 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Sam Reaves on 1/10/19.
+//  Copyright © 2019 Sam Reaves Digital. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+    
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = Date()
+        
+        super.init()
+    }
+    
+    convenience init(random: Bool = false) {
+        if (random) {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spork", "Mac"]
+            
+            var index = arc4random_uniform(UInt32(adjectives.count))
+            let randomAdjective = adjectives[Int(index)]
+            
+            index = arc4random_uniform(UInt32(nouns.count))
+            let randomNoun = nouns[Int(index)]
+            
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Int(arc4random_uniform(100))
+            let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
+            
+            self.init(name: randomName, serialNumber: randomSerialNumber, valueInDollars: randomValue)
+        }
+        else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
+    }
+}
---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Sam Reaves on 1/10/19.
+//  Copyright © 2019 Sam Reaves Digital. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController : UITableViewController {
+    
+}
